19a453ed3f8ca9b4fa5a5ad3901f46e917e8ba50
RPearsonLab <86729351+RPearsonLab@users.noreply.github.com>
2021-07-01 00:04:16 +0100

    Delete diffhigh.m

--- diffhigh.m
+++
@@ -1,5 +0,0 @@
-function [ out ] = diffhigh( matrix, offset )
-matrix_1 = matrix(:,(offset+1):size(matrix,1));
-    matrix_2 = matrix(:, 1:(size(matrix,1)-offset));
-    out = matrix_1 - matrix_2;
-end
